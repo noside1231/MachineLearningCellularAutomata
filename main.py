27533444d66695d1b70eb2a084e9ed00fb267cfa
edisongrauman <egrauman@ucsc.edu>
2017-10-02 22:30:36 -0700

    used on mbp

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import sys
-import scipy
-import numpy
-import matplotlib
-import pandas
-import sklearn
+# import scipy
+# import numpy
+# import matplotlib
+# import pandas
+# import sklearn
 import time
 
 from Grid import Grid
 from Window import Window
 
-totalIterations = 50
+totalIterations = 500
 speed = .05
 
-print("Python: {}".format(sys.version))
-print("scipy: {}".format(scipy.__version__))
-print("numpy: {}".format(numpy.__version__))
-print("matplotlib: {}".format(matplotlib.__version__))
-print("pandas: {}".format(pandas.__version__))
-print("sklearn: {}".format(sklearn.__version__))
+# print("Python: {}".format(sys.version))
+# print("scipy: {}".format(scipy.__version__))
+# print("numpy: {}".format(numpy.__version__))
+# print("matplotlib: {}".format(matplotlib.__version__))
+# print("pandas: {}".format(pandas.__version__))
+# print("sklearn: {}".format(sklearn.__version__))
 print()
 
 g = Grid(100, 100, 5)
-w = Window(1500,400, g)
+w = Window(1500, 400, g)
 
 # grids = [Grid(100, 100, 0) for i in range(1000)]
 
 g.populateGridRandom(1000)
 # g.populateGrid()
 g.show()
-# a = input("go")
+a = input("go")
 lastTime = time.time()
 
 iteration = 0
